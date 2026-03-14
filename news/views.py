718b260013d66f942bcea023fa7109981c6e3d1f
Rolvod <rolvodgames@gmail.com>
2026-03-14 10:29:14 +0500

    Улучшил дизайн и небольшие правки

--- news/views.py
+++ news/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import News
 
+@login_required
 def news_page(request):
     news = News.objects.all()
     return render(request, 'news/news.html', {'news': news})
 
 
+@login_required
 def current_news(request, id):
     news = News.objects.get(id=id)
     return render(request, 'news/current_news.html', {'news': news})
